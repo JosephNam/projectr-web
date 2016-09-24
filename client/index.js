b42c3f974d3bf0323bb953a9b9bb1fbfd1682a3a
Joseph Nam <scholo.temp@gmail.com>
2016-09-24 17:02:58 -0400

    fix small things

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 
-import 'materialize-css/dist/css/materialize.css'
-
 import routes from './routes'
 
 const CustomRouter = () => (
